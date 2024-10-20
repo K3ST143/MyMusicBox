98ec73533e817040b266113db0e286a9608c1292
K3ST <smr_wl@yahoo.com>
2024-10-20 06:16:49 +0300

    Update MyMusicBox.py

--- MyMusicBox.py
+++ MyMusicBox.py
@@ -195,37 +195,5 @@ if __name__ == "__main__":
     
 
 
-# README.md
-# My Music Box
 
-My Music Box is an AI-powered quantum computing processing package integrating next-gen cybersecurity tools.
 
-## Features
-- AI and Quantum Computing with Python
-- Tor VPN for secure connections
-- ProxyChains for anonymous browsing
-- IP Tracer for tracking IP addresses
-- CamPhish for capturing cam shots
-- TruecallerJS for identifying and blocking spam calls
-- f_society for penetration testing tools
-- Vishing, Smishing, App Notification Phishing, Offline Phishing
-- Search for target's full name, addresses, digital profiles, phone numbers, email addresses, live GPS locations, and more
-- Voice recognition over communication mediums
-- Micphishing
-- Tracking devices with anti-spyware installed
-- **Secure communication with encryption**
-- **Automated target searching**
-- **Advanced hacking tools**
-
-#License 
-## MIT License 
-add MIT License 
-
-## Installation
-
-1. Clone the repository and run the automation script:
-   ```sh
-   git clone https://github.com/K3ST143/MyMusicBox.git
-   cd MyMusicBox
-   chmod +x automation.sh
-   ./automation.sh
